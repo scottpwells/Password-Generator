fe192dec3f89893ddc145948d3a824acdae5b149
scottpwells <scottpwells@gmail.com>
2020-09-22 13:40:13 -0400

    added a js script for password generator button

--- password.js
+++ password.js
@@ -1,3 +1,28 @@
 
 
 
+<
+var form = document.getElementById(""); 
+
+/* Function to generate combination of password */ 
+function generateP() { 
+    var pass = ''; 
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
+            'abcdefghijklmnopqrstuvwxyz0123456789@#$'; 
+      
+    for (i = 1; i <= 8; i++) { 
+        var char = Math.floor(Math.random() 
+                    * str.length + 1); 
+          
+        pass += str.charAt(char) 
+    } 
+      
+    return pass; 
+} 
+  
+function gfg_Run() { 
+    el_down.innerHTML = generateP(); 
+} 
+
+
+
